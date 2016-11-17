8ca9471b4dad09593deaf8363c118c85545c5e8a
Andrew Sondey <andrewsondey@gmail.com>
2016-11-17 00:13:10 -0500

    renewed 11/17/16

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
-//visual animation
+//---visual animation
 $(document).ready(function() {
-  $('h1, h2, .nav-bar, .email-me, .header, .center-float-box-1').addClass('animated fadeInDown');
-  $('.social-icons, .contact-form, #vinyl-video').addClass('animated flipInX');
+  $('h1, h2, .nav-bar, .email-me, .header, .header-2, .center-float-box-1').addClass('animated fadeInDown');
+  $('.social-icons, .quote, .vinyl, .contact-form, .underheader-description, .tags-block, .buy-buttons, .about-me').addClass('animated flipInX');
   $('.label-left, .label-right .inner-label').addClass('animated fadeInLeft');
-  $('section, .row-3, .about-me, .contact, .triangle, .center-float-box-2').addClass('animated fadeInUp');
+  $('section, .contact, .triangle, .center-float-box-2').addClass('animated fadeInUp');
   $('.share, .playlist').addClass('animated fadeInUpBig');
 });
 
-//sound animation
-var audio = new Audio('audio/click.mp3');
+//$('.vinyl').on('mouseenter', function() {
+//	event.preventDefault();
+  //$('.vinyl span').css('opacity','0');
+  //$('.vinyl p').fadeTo("slow", 0); 
+//  $('.vinyl p').animate({opacity: 0},500);
+
+//---vinyl animation
+$('.vinyl').on('mouseenter', function (event) {
+	$('.vinyl p').animate({opacity: 0},500);
+    $('.arrow-img').toggleClass('rotate');
+    $('.arrow-img').toggleClass('rotate-reset');
+});
+
+$('.vinyl').on('mouseleave', function() {
+  $('.vinyl p').animate({opacity: 1},3500);
+});
+
+//---sound animation
+var audio = new Audio('../audio/click.mp3');
 $('a').on('mouseenter', function() {
   audio.play();
 });
 
-//share-modal-box
+
+var audio2 = new Audio('audio/click.mp3');
+if( $( "body" ).hasClass( ".home-sound" ) !== true  ){
+	$('a').on('mouseenter', function() {
+  		audio2.play();
+	});
+}
+
+//---share-modal-box
 $(document).ready(function() {
 	$('.share').on('click', function(event){
 		event.preventDefault();
 		$('.overlay').fadeIn(300,
 		 	function(){
-				$('.share-modal-box') 
+				$('.modal-box') 
 					.css('display', 'block')
 					.animate({opacity: 1, top: '50%'}, 200);	
 		});
 	});
 	$('.overlay').on('click', function(){ 
-		$('.share-modal-box')
+		$('.modal-box')
 			.animate({opacity: 0, top: '45%'}, 200, 
 				function(){ 
 					$(this).css('display', 'none'); 
@@ -35,7 +60,7 @@ $(document).ready(function() {
 	});
 });
 
-//categories box
+//---categories box
 $(document).ready(function() {
 	$('.hamburger').on('click', function(event){
 		event.preventDefault();
@@ -60,3 +85,12 @@ $(document).ready(function() {
 			);
 	});
 });
+
+//---email sender code
+$('form').on('submit', function(e){
+	//e.preventDefault();
+	alert("Your Message was sent!");
+	$('input[type="text"], input[type="email"], textarea').val('');
+});
+
+
